64678a5f8d1370917adf26605f7c07cd3ae8c840
BiomimicryCafe <110191082+BiomimicryCafe@users.noreply.github.com>
2022-07-30 18:03:22 -0500

    Delete BLETEST.py

--- BLETEST.py
+++
@@ -1,8 +0,0 @@
-import js2py
-BLE = js2py.require('HACKPACK_Bluetooth') # convert JS lib to python
-if __name__ == '__main__': #main function
-    BLE.start()
-    BLE.send("hello world")
-    while (True):
-        BLE.read()
-        
